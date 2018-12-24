9762be9a3dce054b9977afdbb49543b6a252f17d
arnaud yanga-esso <arnaud.yanga-esso@fullsix.com>
2018-12-24 13:23:44 +0100

    standard

---
+++ components/StandardButton.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo';
+
+class StandardButton extends React.Component {
+    render() {
+        return (
+            <TouchableOpacity  style={{ flex:1, alignItems: 'center'}}>
+                <LinearGradient
+                    colors={['#4c669f', '#3b5998', '#192f6a']}
+                    style={{
+                        marginTop:5,
+                        paddingTop: 5,ss
+                        paddingBottom: 5,
+                        paddingLeft: 15,
+                        paddingRight: 15,
+                        alignItems: 'center',
+                        borderRadius: 5 }}>
+                    <Text
+                        style={{
+                            backgroundColor: 'transparent',
+                            fontSize: 15,
+                            color: '#fff',
+                        }}>
+                        Aléatoire
+                    </Text>
+                </LinearGradient>
+            </TouchableOpacity >
+        );
+    }
+}
+export default StandardButton
