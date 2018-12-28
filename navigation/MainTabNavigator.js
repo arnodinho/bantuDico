d03a9297dda3cc6948b584455b87fb97ca7fdd38
ayangaesso <ayangaesso-ext@lequipe.fr>
2018-12-28 16:26:56 +0100

    icon tabbar

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -18,8 +18,8 @@ HomeStack.navigationOptions = {
       focused={focused}
       name={
         Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
+        ? 'ios-home'
+        : 'md-home'
       }
     />
   ),
@@ -48,7 +48,11 @@ SettingsStack.navigationOptions = {
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+      name={
+        Platform.OS === 'ios'
+          ?  `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-options'
+        }
     />
   ),
 };
