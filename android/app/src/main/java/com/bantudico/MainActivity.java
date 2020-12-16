aeb8aaf5e5a02df67e2924cab915702ff4de47ea
ayanga <arnaud.yanga@believedigital.com>
2020-12-16 09:20:30 +0100

    remove admob

--- android/app/src/main/java/com/bantudico/MainActivity.java
+++ android/app/src/main/java/com/bantudico/MainActivity.java
@@ -10,16 +10,7 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import expo.modules.splashscreen.singletons.SplashScreen;
 import expo.modules.splashscreen.SplashScreenImageResizeMode;
 
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.InterstitialAd;
-
 public class MainActivity extends ReactActivity {
-
-    private InterstitialAd mInterstitialAd;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +18,6 @@ public class MainActivity extends ReactActivity {
     // SplashScreen.show(...) has to be called after super.onCreate(...)
     // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
     SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, ReactRootView.class, false);
-
-      MobileAds.initialize(this, new OnInitializationCompleteListener() {
-          @Override
-          public void onInitializationComplete(InitializationStatus initializationStatus) {
-          }
-      });
-
-      mInterstitialAd = new InterstitialAd(this);
-      mInterstitialAd.setAdUnitId("ca-app-pub-9385763512190012/1734131761");
-      mInterstitialAd.loadAd(new AdRequest.Builder().build());
-
   }
 
 
